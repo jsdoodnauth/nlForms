2db7a1023607eb1b544d7aaecd7f710695ad7fe8
Joshua Doodnauth <jsdoodnauth@gmail.com>
2016-02-23 00:05:55 -0500

    initial commit

---
+++ zensurance/zensurance.js
@@ -0,0 +1,25 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  			
+
+  Template.nlForm.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.nlForm.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
