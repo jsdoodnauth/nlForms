28b7df873f99310c2419c65351d09474ba2fb4bc
Joshua Doodnauth <jsdoodnauth@gmail.com>
2016-02-27 15:10:01 -0500

    updated code

--- zensurance/zensurance.js
+++ zensurance/zensurance.js
@@ -2,7 +2,7 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  			
+
 
   Template.nlForm.helpers({
     counter: function () {
@@ -16,10 +16,69 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+
+  Template.nlForm.events({
+    'change .input1': function(e) {
+        $('.panel2').addClass('show');
+    }
+  });
+
+  Template.nlForm.events({
+    'change .input2': function(e) {
+        if ($('.input2').val() && ($('.input3').val() > 0)) {
+          if ($('.input3').val() == '1') {
+              $('.panel4').addClass('show');
+              $('.panel3').hide();
+          }
+          if ($('.input3').val() == '2') {
+            $('.panel3').removeAttr('style','display:none').addClass('show');
+            $('.panel4').removeClass('show');
+          }
+        }
+      }
+  });
+
+  Template.nlForm.events({
+    'change .input3': function(e) {
+        if ($('.input2').val() && ($('.input3').val() > 0)) {
+          if ($('.input3').val() == '1') {
+              $('.panel4').addClass('show');
+              $('.panel3').hide();
+          }
+          if ($('.input3').val() == '2') {
+            $('.panel3').removeAttr('style','display:none').addClass('show');
+            $('.panel4').removeClass('show');
+          }
+        }
+    }
+  });
+
+  Template.nlForm.events({
+    'keyup .input4': function(e) {
+        $('.input4').val(commaSeparateNumber($('.input4').val()));
+    }
+  });
+
+  Template.nlForm.events({
+    'change .input4': function(e) {
+        $('.panel4').addClass('show');
+    }
+  });
+
+  Template.nlForm.events({
+    'change .input5': function(e) {
+        $('.panel5').addClass('show');
+    }
+  });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+}
+
+function commaSeparateNumber(num){
+  var out = num.replace(/\,/g,"");
+  return out.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
 }
