546de35a4c47dd7fd12245709200f0deeb5efcbf
Joshua Doodnauth <jsdoodnauth@gmail.com>
2016-02-27 22:52:50 -0500

    updated code

--- zensurance/zensurance.js
+++ zensurance/zensurance.js
@@ -1,74 +1,93 @@
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
-
-
+  var cityAvilable = ["toronto","hamilton","ottawa"];
+  var industryAvilable = ["accounting","advertising","aerospace","architecture","banking","business","computer","financial","insurance","medical","medicine","retail","technology" ];
 
   Template.nlForm.helpers({
     counter: function () {
       return Session.get('counter');
-    }
+    },
+    cityCollection: [{name: "toronto"}, {name: "hamilton"}, {name: "ottawa"}]
   });
 
   Template.nlForm.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-
-  Template.nlForm.events({
-    'change .input1': function(e) {
+    'change .inputCompany': function(e) {
+      //Session.set('counter', Session.get('counter') + 1);
         $('.panel2').addClass('show');
-    }
-  });
+    },
 
-  Template.nlForm.events({
-    'change .input2': function(e) {console.log('hit2');
-        if ($('.input2').val() && ($('.input3').val() > 0)) {
-          if ($('.input3').val() == '1') {
-              $('.panel4').addClass('show');
-              $('.panel3').hide();
+    'change .inputCity': function(e) {console.log('hit2');
+        if ($('.inputCity').val() && ($('.inputStatus').val() > 0)) {
+          var city = $(".inputCity").val().toLowerCase();
+          var found = $.inArray(city, cityAvilable) > -1;
+          if (found) {
+            if ($('.inputStatus').val() == '1') {
+                $('.panel3').hide();
+                $('.panel4').removeAttr('style','display:none').addClass('show');
+                $('.panelemail').removeClass('show');
+            }
+            if ($('.inputStatus').val() == '2') {
+              $('.panel3').removeAttr('style','display:none').addClass('show');
+              $('.panel4').removeClass('show');
+              $('.panelemail').removeClass('show');
+            }
           }
-          if ($('.input3').val() == '2') {
-            $('.panel3').removeAttr('style','display:none').addClass('show');
-            $('.panel4').removeClass('show');
-          }
-        }
-      }
-  });
-
-  Template.nlForm.events({
-    'change .input3': function(e) {console.log('hit3');
-        if ($('.input2').val() && ($('.input3').val() > 0)) {
-          if ($('.input3').val() == '1') {
-              $('.panel4').addClass('show');
-              $('.panel3').hide();
-          }
-          if ($('.input3').val() == '2') {
-            $('.panel3').removeAttr('style','display:none').addClass('show');
-            $('.panel4').removeClass('show');
+          else {
+            $('.panel3').hide();
+            $('.panel4').hide();
+            $('.panelemail').addClass('show');
           }
         }
-    }
-  });
+      },
 
-  Template.nlForm.events({
-    'keyup .input4': function(e) {
-        $('.input4').val(commaSeparateNumber($('.input4').val()));
-    }
-  });
+    'change .inputStatus': function(e) {console.log('hit3');
+        if ($('.inputCity').val() && ($('.inputStatus').val() > 0)) {
+          var city = $(".inputCity").val().toLowerCase();
+          var found = $.inArray(city, cityAvilable) > -1;
+          if (found) {
+            if ($('.inputStatus').val() == '1') {
+                $('.panel3').hide();
+                $('.panel4').removeAttr('style','display:none').addClass('show');
+                $('.panelemail').removeClass('show');
+            }
+            if ($('.inputStatus').val() == '2') {
+              $('.panel3').removeAttr('style','display:none').addClass('show');
+              $('.panel4').removeClass('show');
+              $('.panelemail').removeClass('show');
+            }
+          }
+          else {
+            $('.panel3').hide();
+            $('.panel4').hide();
+            $('.panelemail').addClass('show');
+          }
+        }
+    },
 
-  Template.nlForm.events({
-    'change .input4': function(e) {
+    'keyup .inputRev': function(e) {
+        $('.inputRev').val(commaSeparateNumber($('.inputRev').val()));
+    },
+    'change .inputRev': function(e) {
       console.log('hit4');
         $('.panel4').addClass('show');
-    }
-  });
+    },
 
-  Template.nlForm.events({
-    'change .input5': function(e) {
-        $('.panel5').addClass('show');
+    'change .inputIndustry': function(e) {
+        var industry = $(".inputIndustry").val().toLowerCase();
+        var found = $.inArray(industry, industryAvilable) > -1;
+        if (found) {
+          $('.panelsubmit').addClass('show');
+          $('.panelemail').removeClass('show').hide();
+        }
+        else {
+          $('.panelemail').removeAttr('style','display:none').addClass('show');
+          $('.panelsubmit').removeClass('show');
+        }
+    },
+
+    'change .panelemail': function(e) {
+      $('.panelsubmit').addClass('show');
     }
   });
 }
