62e57e8e98ead2abadeb5f0472ade659efb04852
Joshua Doodnauth <jsdoodnauth@gmail.com>
2016-03-08 00:01:35 -0500

    updated code

--- zensurance/createQuote.js
+++ zensurance/createQuote.js
@@ -4,7 +4,7 @@ if (Meteor.isClient) {
     'click #btnNextStep': function(e) {
       var stepClass = document.getElementById('hidSteps').value;
       stepClass++;
-      StepController(stepClass);
+      StepController(stepClass, 1);
 
       $('.progress-steps-container ul.progress-steps li.step' + stepClass).addClass('active');
       $('.progress-steps-container ul.progress-steps li.step' + (stepClass-1)).addClass('complete');
@@ -12,7 +12,7 @@ if (Meteor.isClient) {
     'click #btnBackStep': function(e) {
       var stepClass = document.getElementById('hidSteps').value;
       stepClass--;
-      StepController(stepClass);
+      StepController(stepClass, -1);
 
       $('.progress-steps-container ul.progress-steps li.step' + (stepClass+1)).removeClass('active').removeClass('complete');
       $('.progress-steps-container ul.progress-steps li.step' + stepClass).removeClass('complete').addClass('active');
@@ -20,7 +20,7 @@ if (Meteor.isClient) {
     'click .carrier-selection-list > ul > li': function(e) {
       var stepClass = document.getElementById('hidSteps').value;
       stepClass++;
-      StepController(stepClass);
+      StepController(stepClass, 1);
 
       $('.progress-steps-container ul.progress-steps li.step' + (stepClass+1)).removeClass('active').removeClass('complete');
       $('.progress-steps-container ul.progress-steps li.step' + stepClass).removeClass('complete').addClass('active');
@@ -28,7 +28,7 @@ if (Meteor.isClient) {
     'click #btnPayment': function(e) {
       var stepClass = document.getElementById('hidSteps').value;
       stepClass++;
-      StepController(stepClass);
+      StepController(stepClass, 1);
 
       $('.progress-steps-container ul.progress-steps li.step' + (stepClass+1)).removeClass('active').removeClass('complete');
       $('.progress-steps-container ul.progress-steps li.step' + stepClass).removeClass('complete').addClass('active');
@@ -36,22 +36,31 @@ if (Meteor.isClient) {
     'click .btnAdd': function(e) {
       var btn = $(e.target);
       if (btn.hasClass('active')) {
-        var text = btn.val();
-        var preText = $('.services-added').html();
-        preText= preText.replace(text + "<br>", "");
-        $('.services-added').html(preText);
-        if (preText.length < 1) {
-          $('.services-added-sub').addClass("hide");
+        if (btn.hasClass('btnAdd1')) { $('.services-added1').removeClass('active').fadeOut(); }
+        if (btn.hasClass('btnAdd2')) { $('.services-added2').removeClass('active').fadeOut(); }
+        if (btn.hasClass('btnAdd3')) { $('.services-added3').removeClass('active').fadeOut(); }
+        if (btn.hasClass('btnAdd4')) { $('.services-added4').removeClass('active').fadeOut(); }
+        if (btn.hasClass('btnAdd5')) { $('.services-added5').removeClass('active').fadeOut(); }
+        if (btn.hasClass('btnAdd6')) { $('.services-added6').removeClass('active').fadeOut(); }
+        if ($('.services-added > div.active').length < 1) {
+          $('.progress-bar-section').fadeOut();
+          $('.coverage-info-text').fadeIn();
+          $('#btnNextStep').fadeOut();
           $('.product-comparison-list').removeClass('active');
         }
         btn.html('Add +').removeClass('active');
       }
       else {
-        var text = btn.val();
-        var preText = $('.services-added').html();
-        $('.services-added').html(preText + "" + text + "<br />");
-        $('.services-added-sub').removeClass("hide");
+        if (btn.hasClass('btnAdd1')) { $('.services-added1').addClass('active').fadeIn(); }
+        if (btn.hasClass('btnAdd2')) { $('.services-added2').addClass('active').fadeIn(); }
+        if (btn.hasClass('btnAdd3')) { $('.services-added3').addClass('active').fadeIn(); }
+        if (btn.hasClass('btnAdd4')) { $('.services-added4').addClass('active').fadeIn(); }
+        if (btn.hasClass('btnAdd5')) { $('.services-added5').addClass('active').fadeIn(); }
+        if (btn.hasClass('btnAdd6')) { $('.services-added6').addClass('active').fadeIn(); }
         $('.product-comparison-list').addClass('active');
+        $('.coverage-info-text').fadeOut();
+        $('.progress-bar-section').fadeIn();
+        $('#btnNextStep').fadeIn();
         btn.addClass('active').html("<img src='img/checkmark.png' alt='' />");
       }
     },
@@ -85,7 +94,7 @@ if (Meteor.isServer) {
 }
 
 
-function StepController(stepClass) {
+function StepController(stepClass, direction) {
       // 1 -> .services-container
       // 2 -> .companyStructure-container
       // 3 -> .companyLocation-container
@@ -94,60 +103,66 @@ function StepController(stepClass) {
       // 6 -> .carrier-selection-container
       // 7 -> .payment-container
       // 8 -> .completion-container
-  var DELAY = 1000;
   switch (stepClass) {
     case 1:
-      $('.companyStructure-container').removeClass('active').fadeOut('fast', function() {
-        $('.services-container').delay(DELAY).addClass('active').fadeIn();
-        console.log('animation done');
-      });
-
+      if (direction > 0) {
+        animate(".companyStructure-container", 'fadeOutRight', 'hide', '.services-container', 'fadeInLeft'); //Forward
+      }
+      else {
+        animate(".companyStructure-container", 'fadeOutLeft', 'hide', '.services-container', 'fadeInRight'); //Back Step
+      }
       $('#btnBackStep').addClass('hide');
       break;
     case 2:
-      $('.companyLocation-container').removeClass('active').fadeOut();
-      $('.services-container').removeClass('active').fadeOut('fast', function() {
-        $('.companyStructure-container').delay(DELAY).addClass('active').fadeIn();
-      });
+      if (direction > 0) {
+        animate(".services-container", 'fadeOutRight', 'hide', '.companyStructure-container', 'fadeInLeft'); //Forward
+      }
+      else {
+        animate(".companyLocation-container", 'fadeOutLeft', 'hide', '.companyStructure-container', 'fadeInRight'); //Back Step
+      }
       $('#btnBackStep').removeClass('hide');
       break;
     case 3:
-      $('.companyOperations-container').removeClass('active').fadeOut();
-      $('.companyStructure-container').removeClass('active').fadeOut('fast', function() {
-          $('.companyLocation-container').delay(DELAY).addClass('active').fadeIn();
-      });
+      if (direction > 0) {
+        animate(".companyStructure-container", 'fadeOutRight', 'hide', '.companyLocation-container', 'fadeInLeft'); //Forward
+      }
+      else {
+        animate(".companyOperations-container", 'fadeOutLeft', 'hide', '.companyLocation-container', 'fadeInRight'); //Back Step
+      }
       break;
     case 4:
-      $('.finalizeService-container').removeClass('active').fadeOut();
-      $('.companyLocation-container').removeClass('active').fadeOut('fast', function() {
-          $('.companyOperations-container').delay(DELAY).addClass('active').fadeIn();
-      });
-
+      if (direction > 0) {
+        animate(".companyLocation-container", 'fadeOutRight', 'hide', '.companyOperations-container', 'fadeInLeft'); //Forward
+      }
+      else {
+        animate(".finalizeService-container", 'fadeOutLeft', 'hide', '.companyOperations-container', 'fadeInRight'); //Back Step
+      }
       break;
     case 5:
-      $('.companyOperations-container').removeClass('active').fadeOut('fast', function() {
-          $('.finalizeService-container').delay(DELAY).addClass('active').fadeIn();
-      });
-
+      if (direction > 0) {
+        animate(".companyOperations-container", 'fadeOutRight', 'hide', '.finalizeService-container', 'fadeInLeft'); //Forward
+      }
+      else {
+        animate(".companyOperations-container", 'fadeOutLeft', 'hide', '.finalizeService-container', 'fadeInRight'); //Back Step
+      }
       break;
     case 6:
-      $('.finalizeService-container').removeClass('active').fadeOut('fast', function() {
-          $('.left-panel.service-selection-container').fadeOut();
-          $('.right-panel').fadeOut();
-          $('.carrier-selection-container').delay(DELAY).addClass('active').fadeIn();
-      });
+      if (direction > 0) {
+        animate(".finalizeService-container", 'fadeOut', 'hide', '.carrier-selection-container', 'fadeIn'); //Forward
+        $('.left-panel.service-selection-container').fadeOut();
+        $('.right-panel').fadeOut();
+      }
       break;
     case 7:
-      $('.carrier-selection-container').removeClass('active').fadeOut('fast', function() {
-          $('.payment-container').delay(DELAY).addClass('active').fadeIn();
-      });
-
+      if (direction > 0) {
+        animate(".carrier-selection-container", 'fadeOut', 'hide', '.payment-container', 'fadeIn'); //Forward
+      }
       break;
     case 8:
-      $('.payment-container').removeClass('active').fadeOut('fast', function() {
-          $('.completion-container').delay(DELAY).addClass('active').fadeIn();
-      });
-      $('html').css('background-color','#FFF');
+      if (direction > 0) {
+        animate(".payment-container", 'fadeOut', 'hide', '.completion-container', 'fadeIn'); //Forward
+      }
+//      $('html').css('background-color','#FFF');
       break;
     default:
     break;
@@ -162,4 +177,16 @@ function setProgressPercent(val) {
   var percent = val;
   $('.progress-bar-value .val').html(percent + '%');
   $('.progress-bar-completed').css('width', percent + '%');
+}
+
+function animate(outgoingElement, animationOut, state, activeElement, animationIn) {
+    $(outgoingElement).addClass(animationOut);
+    var wait = window.setTimeout( function(){
+        $(outgoingElement).hide();
+        $(activeElement).show().addClass(animationIn);
+        $(outgoingElement).removeClass(animationOut)}, 300
+    );
+    var wait2 = window.setTimeout( function(){
+        $(activeElement).removeClass(animationIn)}, 600
+    );
 }
